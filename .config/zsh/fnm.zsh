b002aebbcaad1573da88b631adf2f01660866e29
Ted Kulp <ted@tedkulp.com>
2021-11-12 06:42:22 -0500

    More fnm tweaks

--- .config/zsh/fnm.zsh
+++ .config/zsh/fnm.zsh
@@ -1,4 +1,14 @@
 export PATH=/home/ted/.fnm:$PATH
 eval "`fnm env`"
 
+autoload -U add-zsh-hook
+_fnm_autoload_hook () {
+    if [[ -f .node-version || -f .nvmrc ]]; then
+        fnm use --install-if-missing
+    fi
+}
+
+add-zsh-hook chpwd _fnm_autoload_hook \
+    && _fnm_autoload_hook
+
 alias nvm="fnm"
