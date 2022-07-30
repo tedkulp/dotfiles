e4ecd4c42200b477dba4740e0bc97a415ed8eed4
Ted Kulp <ted.kulp@wizehive.com>
2022-07-30 09:01:35 -0400

    fix: disable lsp lines on startup

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -403,6 +403,7 @@ lvim.plugins = {
     "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
     config = function()
       require("lsp_lines").setup()
+      vim.diagnostic.config { virtual_lines = false, virtual_text = true }
     end,
   },
   {
