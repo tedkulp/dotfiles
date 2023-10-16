a661e109ce4834eac3a98b2ed7e6a11ad7450c0f
Ted Kulp <ted@tedkulp.com>
2023-10-16 06:54:30 -0400

    fix: finally got things back to normal

--- .config/zsh/vim.zsh
+++ .config/zsh/vim.zsh
@@ -1,10 +1,1 @@
-if [[ -f "/usr/local/bin/gcc-12" ]]; then # Mac Hack for building vim modules
-  alias vim="CC=/usr/local/bin/gcc-12 nvim"
-  alias vi="CC=/usr/local/bin/gcc-12 nvim"
-else
-  alias vim="nvim"
-  alias vi="nvim"
-fi
-
-export EDITOR="nvim"
-alias nvim="nvm use v16 && /Users/tedkulp/.asdf/shims/nvim"
+export EDITOR=nvim
