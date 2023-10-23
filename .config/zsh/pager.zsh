36813854c97655d159ec71132151672a94c30caa
Ted Kulp <ted@tedkulp.com>
2023-10-23 14:58:24 -0400

    feat: pager

---
+++ .config/zsh/pager.zsh
@@ -0,0 +1,15 @@
+function p () {
+  local pager="$PAGER"
+
+  if [ -n "$pager" ]; then
+    if command -v "$pager" &>/dev/null; then
+      $PAGER
+    fi
+  else
+    if command -v less &>/dev/null; then
+      less -R
+    else
+      cat
+    fi
+  fi
+}
