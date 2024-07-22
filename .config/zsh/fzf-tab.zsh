0d5df845c88b6f3990575e61c4495231946cb4b4
Ted Kulp <ted@tedkulp.com>
2024-07-22 07:26:21 -0400

    use eza and a few other changes

--- .config/zsh/fzf-tab.zsh
+++ .config/zsh/fzf-tab.zsh
@@ -1,6 +1,9 @@
 # https://github.com/Aloxaf/fzf-tab?tab=readme-ov-file#fzf-tab
 
 zstyle ':completion:*:descriptions' format '[%d]'
-zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+zstyle ':fzf-tab:*' fzf-command 'fzf-tmux'
+zstyle ':fzf-tab:*' fzf-flags '-p 75%'
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-
+zstyle ':completion:*' menu no
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+zstyle ':fzf-tab:*' switch-group '<' '>'
