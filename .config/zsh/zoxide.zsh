854eafde2646d7e0137b3e0af480743e13456786
Napsack Builder <napsack-builder@anthropic.com>
2025-06-30 09:36:41 -0400

    Make sure zoxide is setup before using it for `cd`

--- .config/zsh/zoxide.zsh
+++ .config/zsh/zoxide.zsh
@@ -1,1 +1,8 @@
-alias cd=z
+# alias cd=z
+cd() {
+  if whence -w z >/dev/null 2>&1; then # Does a command called 'z' exist right now?
+    z "$@"                             # use it
+  else
+    builtin cd "$@"                    # quiet fallback
+  fi
+}
